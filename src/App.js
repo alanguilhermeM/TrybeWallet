76379408a9f19f0ff7599bb37641a9270de4ddbe
alanguilhermeM <alanguilherme13@outlook.com>
2023-05-12 14:00:37 -0300

    Finalizado requisito 1 e começando 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <div>
+      <Switch>
+        <Login exact path="/" component={ Login } />
+        <Wallet path="/carteira" component={ Wallet } />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,80 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addEmail } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  validation = () => {
+    const { email, password } = this.state;
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const valido = regex.test(email);
+    const n = 5;
+    return !(valido === true && password.length > n);
+  };
+
+  handleClick = () => {
+    const { email } = this.state;
+    const { dispatch, history } = this.props;
+    dispatch(addEmail(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <div>
+        Login
+        <label>
+          Email:
+          <input
+            value={ email }
+            name="email"
+            type="email"
+            data-testid="email-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            value={ password }
+            name="password"
+            type="password"
+            data-testid="password-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          onClick={ this.handleClick }
+          disabled={ this.validation() }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: PropTypes.func.isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  email: state.email,
+});
+
+export default connect(mapStateToProps)(withRouter(Login));
